037c3329f6cf6bc2d41686309f25ea752f8f4c15
lph15925637437 <1409531757@qq.com>
2019-06-04 09:52:09 +0800

    '工具类的完善'

--- src/main/java/com/lph/rsa/exception/FormatErrorEnum.java
+++ src/main/java/com/lph/rsa/exception/FormatErrorEnum.java
@@ -1,16 +1,18 @@
 package com.lph.rsa.exception;
-/**  
-* @Description:  
- * @Author: dingjl  
-* @Date: 2018年4月12日  
-**/
+
+
+/**
+ *
+ * @author: lph
+ * @date:  2019/6/4 9:51
+ * @version V1.0
+ */
 public enum FormatErrorEnum {
     Date_Format_Error("F10001","日期转换出现异常!"),
     Date_Format_IsNull("F10002","日期不能为空!"),
     Paramer_Format_Error("F10003","参数格式错误!"),
     Paramer_Format_IsNull("F10004","参数不能为空!"),
     Paramer_Format_length("F10005","参数长度过长!"),
-    Passage_IsNull("F10006", "应用所属通道不能为空"),
     ;
     private String errorCode;
     
